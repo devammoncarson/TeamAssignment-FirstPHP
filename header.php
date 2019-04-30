bffb558ce1e18e05df17f5f161abbb489664bb04
Ammon Carson <pianoace1315@gmail.com>
2019-04-30 16:39:57 -0600

    Added Php

--- header.php
+++ header.php
@@ -1,7 +1,13 @@
+<?php 
+
+$file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
+
+?>
+
 <header>
 	<h2>Think.</h2>
 		<ul>
-			<li><a href="/home.php">Home</a></li>
+			<li><a <?php if ($file === "home") echo 'active' ?>href="/home.php">Home</a></li>
 			<li><a href="/about-us.php">About Us</a></li>
 			<li><a href="/login.php">login</a></li>
 		</ul>
